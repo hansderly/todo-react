1e4d21bb277f584c7d4ebe22fe012a1bd131735d
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-02-20 12:59:35 -0400

    add prop and propTypes to ItemInput component

--- src/components/ItemInput/ItemInput.jsx
+++ src/components/ItemInput/ItemInput.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { PlusOutlined } from '@ant-design/icons';
 
 import styles from './ItemInput.module.css';
 
-function ItemInput() {
+function ItemInput({ value, onChange }) {
   return (
     <div className={styles.container}>
-      <input className={styles.input} type="text" name="todo" id="todo" placeholder="Add todo..." />
+      <input value={value} onChange={onChange} className={styles.input} type="text" name="todo" id="todo" placeholder="Add todo..." />
       <PlusOutlined className={styles.icon} />
     </div>
   );
 }
+
+ItemInput.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
 
 export default ItemInput;
