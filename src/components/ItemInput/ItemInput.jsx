94f21d076d2de2edfb630cbe657ec4defbf3922a
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-02-20 13:18:08 -0400

    add onKeyDown function to Item input and sample funtion to hande that event

--- src/components/ItemInput/ItemInput.jsx
+++ src/components/ItemInput/ItemInput.jsx
@@ -4,10 +4,10 @@ import { PlusOutlined } from '@ant-design/icons';
 
 import styles from './ItemInput.module.css';
 
-function ItemInput({ value, onChange }) {
+function ItemInput({ value, onChange, onKeyDown }) {
   return (
     <div className={styles.container}>
-      <input value={value} onChange={onChange} className={styles.input} type="text" name="todo" id="todo" placeholder="Add todo..." />
+      <input value={value} onChange={onChange} onKeyDown={onKeyDown} className={styles.input} type="text" name="todo" id="todo" placeholder="Add todo..." />
       <PlusOutlined className={styles.icon} />
     </div>
   );
@@ -16,6 +16,7 @@ function ItemInput({ value, onChange }) {
 ItemInput.propTypes = {
   value: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
+  onKeyDown: PropTypes.func.isRequired,
 };
 
 export default ItemInput;
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import styles from './ItemList.module.css';
 
@@ -7,10 +7,16 @@ import ItemListHeader from '../ItemListHeader/ItemListHeader';
 import ItemInput from '../ItemInput/ItemInput';
 
 function ItemList() {
+  const [value, setValue] = useState('');
+
+  function addTodo(todo) {
+    setValue('');
+  }
+
   return (
     <div className={styles.container}>
       <ItemListHeader />
-      <ItemInput />
+      <ItemInput value={value} onChange={(e) => setValue(e.target.value)} onKeyDown={(e) => (e.key === 'Enter' ? addTodo(value) : null)} />
       <Item />
     </div>
   );
