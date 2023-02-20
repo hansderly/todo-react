79435f1f0ccac8dab72d8b59f51a3f0443c5cd0c
Hans-Derly Rameau <rameauhansderly@gmail.com>
2023-02-20 12:42:14 -0400

    add item todo boilerplate

--- src/components/Item/intem.jsx
+++ src/components/Item/intem.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { DeleteOutlined } from '@ant-design/icons';
+
+import styles from './Item.module.css';
 
 function Item() {
   return (
-    <div>Item</div>
+    <div className={styles.container}>
+      <div>
+        <input type="checkbox" name="complete" id="complete" />
+        <p>Todo name</p>
+      </div>
+      <DeleteOutlined />
+    </div>
   );
 }
 
